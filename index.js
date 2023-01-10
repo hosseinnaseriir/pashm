64b366214159806bbd6d57b87724878e38818498
Hossein <hossein19naseri99@gmail.com>
2023-01-10 18:03:05 +0330

    Add guide to the project

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Hi Git');
+
+// git install
+// git --version
+// git init
+// git add -A
+// git commit -m "commit message"
+// create a repo in github
+// git remote add origin /github repo address/
+// git push origin branchName
+
